8691a2c8e052e1b40d320a1d3c97f44dd21f7be5
SamruddhiTivale <samtivale@gmail.com>
2020-08-03 18:49:56 +0530

    HomeActivity rename with HomePageAcitvity

--- app/src/main/java/com/example/rajakakamall/login/RegisterActivity.java
+++ app/src/main/java/com/example/rajakakamall/login/RegisterActivity.java
@@ -2,9 +2,11 @@ package com.example.rajakakamall.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,14 +16,21 @@ import com.example.rajakakamall.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class RegisterActivity extends AppCompatActivity {
+
+  private static final String TAG = "PhoneAuth";
+
 
   private FirebaseAuth mAuth;
   String otpSent;
@@ -69,6 +78,17 @@ public class RegisterActivity extends AppCompatActivity {
     mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
       @Override
       public void onComplete(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+          loginBtn.setEnabled(true);
+          enterOtp.setText("");
+          FirebaseUser user = task.getResult().getUser();
+
+        } else {
+          if (task.getException() instanceof
+                  FirebaseAuthInvalidCredentialsException) {
+            // The verification code entered was invalid
+          }
+        }
         Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
         startActivity(intent);
       }
@@ -99,10 +119,26 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
 
+      loginBtn.setEnabled(true);
+
+        //Getting otp for auto verification
+        enterOtp.setText(Objects.requireNonNull(phoneAuthCredential.getSmsCode()));
+        signInWithPhoneAuthCredential(phoneAuthCredential);
+        Toast.makeText(RegisterActivity.this, "Verified..", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public void onVerificationFailed(FirebaseException e) {
+
+      if (e instanceof FirebaseAuthInvalidCredentialsException) {
+        // Invalid request
+        Log.d(TAG, "Invalid credential: "
+                + e.getLocalizedMessage());
+      } else if (e instanceof FirebaseTooManyRequestsException) {
+        // SMS quota exceeded
+        Log.d(TAG, "SMS Quota exceeded.");
+      }
 
     }
 
