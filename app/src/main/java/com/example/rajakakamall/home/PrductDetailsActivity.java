ed341223bef69b5304d5c8917f7fff09f8184a40
SamruddhiTivale <samtivale@gmail.com>
2020-08-04 10:45:27 +0530

    HomeActivity rename with HomePageAcitvity

--- app/src/main/java/com/example/rajakakamall/home/PrductDetailsActivity.java
+++ app/src/main/java/com/example/rajakakamall/home/PrductDetailsActivity.java
@@ -1,6 +1,11 @@
 package com.example.rajakakamall.home;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -12,6 +17,7 @@ import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PrductDetailsActivity extends AppCompatActivity {
 
@@ -20,6 +26,7 @@ public class PrductDetailsActivity extends AppCompatActivity {
     private ViewPager viewPagerImages;
     private TabLayout tabLayoutImages;
     private FloatingActionButton wishlistbtn;
+    private TextView txttitle,txtprice,txtmrp;
 
 
 
@@ -28,17 +35,40 @@ public class PrductDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prduct_details);
 
+        txttitle=(TextView)findViewById(R.id.txtproductdetailstitle);
+        txtprice=(TextView)findViewById(R.id.txtproductdetailsprice);
+        txtmrp=(TextView)findViewById(R.id.txtproductdetailsmrp);
         viewPagerImages=(ViewPager)findViewById(R.id.viewpager_product_images);
         tabLayoutImages=(TabLayout)findViewById(R.id.tablayout_view_pager);
 
-        wishlistbtn=(FloatingActionButton)findViewById(R.id.floatingActionButton);
+        wishlistbtn=(FloatingActionButton)findViewById(R.id.detailsfloatingActionButton);
         tabLayoutImages.setupWithViewPager(viewPagerImages,true);
 
+        setDetails();
+
+    }
+
+    public void setDetails()
+    {
         List<Integer> productImages = new ArrayList<>();
-        productImages.add(R.drawable.img1);
+
+        Intent intent=getIntent();
+        int productImage=intent.getExtras().getInt("ProductImage");
+
+        productImages.add(productImage);
         productImages.add(R.drawable.img2);
         productImages.add(R.drawable.img3);
         productImages.add(R.drawable.img4);
+
+        String title= Objects.requireNonNull(intent.getExtras()).getString("Title");
+        txttitle.setText(title);
+
+        Double price=intent.getExtras().getDouble("Price");
+        txtprice.setText(String.valueOf(price));
+
+        Double mrp=intent.getExtras().getDouble("MRP");
+        txtmrp.setText(String.valueOf(mrp));
+
 
         ProductImagesAdapter productImagesAdapter=new ProductImagesAdapter(productImages);
         viewPagerImages.setAdapter(productImagesAdapter);
@@ -47,11 +77,18 @@ public class PrductDetailsActivity extends AppCompatActivity {
 
     public void addedtoWishlist()
     {
-        if(ADD_TO_WISHLIST)
-        {
-
-        }
-        else {}
+       wishlistbtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               if (ADD_TO_WISHLIST)
+               {
+                   wishlistbtn.setSupportImageTintList(ColorStateList.valueOf(Color.parseColor("#708090")));
+               }
+               else{
+                   wishlistbtn.setSupportImageTintList(getResources().getColorStateList(R.color.colorAccent));
+               }
+           }
+       }) ;
 
     }
 }
